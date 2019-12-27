46c3a3ef31914412e56b17a0e95f6415b4223969
Yuqi Liu <yuqi.lyle@outlook.com>
2019-12-28 02:45:49 +0800

    initialize
    	new file:   .gitignore
    	new file:   init.el

---
+++ init.el
@@ -0,0 +1,11 @@
+
+(add-to-list 'load-path (concat user-emacs-directory "lisp"))
+
+(require 'init-path)
+(require 'init-package)
+(require 'init-frame)
+(require 'init-general)
+(require 'init-editing)
+(require 'init-company)
+(require 'init-magit)
+(require 'init-slime)
