927c61a173306c1f48064e67ac0c935b9913c2ca
Yuqi Liu <yuqi.lyle@outlook.com>
2019-12-28 14:47:43 +0800

    add c/c++ environment

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,10 +1,15 @@
 (provide 'init-company)
 
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
+
 (require-package 'company)
 
 (add-hook 'after-init-hook 'global-company-mode)
 
 (with-eval-after-load 'company
-  (define-key company-active-map (kbd "M-/") 'company-complete)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous))
+    (dolist (backend '(company-eclim company-semantic))
+      (delq backend company-backends))
+    (define-key company-active-map (kbd "M-/") 'company-complete)
+    (define-key company-active-map (kbd "C-n") 'company-select-next)
+    (define-key company-active-map (kbd "C-p") 'company-select-previous))
