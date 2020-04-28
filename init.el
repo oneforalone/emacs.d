8bdb053b837ae32c0dd390eb9dcb0b357ff869c5
Ritchie <yuqi.lyle@outlook.com>
2020-04-28 15:30:35 +0800

    update

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+;; Make startup faster by reducing the frequency of garbage
+;; collection.  The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; The rest of the init file.
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
 
 (add-to-list 'load-path (expand-file-name "lisp"  user-emacs-directory))
 
@@ -9,11 +16,11 @@
 (require 'init-company)
 (require 'init-magit)
 (require 'init-slime)
-(require 'init-paredit) 
-(require 'init-c-setups)
-(require 'init-latex)
-(require 'init-org)
+(require 'init-paredit)
+(require 'init-cdev)
 (require 'init-rust)
+(require 'init-org)
+;; ;; (require 'init-latex)
 
 (when (file-exists-p custom-file)
   (load custom-file))
