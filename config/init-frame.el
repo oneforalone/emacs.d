cb34bfc4dbe32d3af407294ddd331bd564fe0655
Yuqi Liu <yuqi.lyle@outlook.com>
2020-09-14 14:48:39 +0800

    'remove awesome-tray and awesome-tab plugin'

--- config/init-frame.el
+++ config/init-frame.el
@@ -10,28 +10,6 @@
 
 (setq inhibit-startup-screen t)
 
-;; doom-modeline fit Microsoft Windows Well, but not gnu/linux, nor Mac OS.
-(if (eq system-type 'windows-nt)
-    (use-package doom-modeline
-    :defer t
-    :ensure t
-    :init (add-hook 'after-init-hook #'doom-modeline-mode))
-  (use-package awesome-tray
-      :init
-      (add-hook 'after-init-hook #'awesome-tray-enable)
-      (setq awesome-tray-active-modules
-	    '("location" "git" "parent-dir" "mode-name" "battery" "date"))))
-
-(use-package awesome-tab
-    :config
-      (awesome-tab-mode 1)
-      (setq awesome-tab-height 100)
-    :bind (("s-h" . awesome-tab-backward-tab)
-    	   ("s-j" . awesome-tab-forward-group)
-    	   ("s-k" . awesome-tab-backward-group)
-    	   ("s-l" . awesome-tab-forward-tab)))
-
-
 ;; for windows control
 (defun window-moving-keybind ()
   (global-set-key (kbd "C-s-h") 'windmove-up)
